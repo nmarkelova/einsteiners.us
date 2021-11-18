dcb5dcc843e3bf0f18ed0469cdb2fd15deedab39
einsteinersnet <90713725+einsteinersnet@users.noreply.github.com>
2021-11-18 14:49:37 +0400

    Edit Model Agreements

--- app/Models/Agreement.php
+++ app/Models/Agreement.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Agreement extends Model
 {
+    protected $fillable = [
+        'name',
+        'cover_path',
+    ];
+    
     use HasFactory;
 }
